452ef8fa4a01898b6c7500b926a77960dd5b4c67
Hebert <hebertdev82@gmail.com>
2026-05-21 15:29:13 -0300

    fix: removing comments

--- app/page.js
+++ app/page.js
@@ -2,7 +2,7 @@
 
 import { useEffect, useState } from 'react'
 import { useRouter } from 'next/navigation'
-import { supabase } from './lib/supabase/client' // Ajuste o caminho do seu client se necessário
+import { supabase } from './lib/supabase/client'
 import Sidebar from "./components/sidebar/page";
 import Main from "./components/main/page";
 
